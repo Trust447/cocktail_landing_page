03d1f300363af98fcf91d62888903af47cffbf31
Trust Ihemebiri <isaactrust447@gmail.com>
2025-07-15 22:24:02 +0100

    shayo brand

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React from "react"
+import gsap from 'gsap';
+import { ScrollTrigger, SplitText } from "gsap/all";
+
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import Cocktails from './components/Cocktails.jsx'
+import About from './components/About.jsx'
+import Art from './components/Art.jsx'
+import Menu from './components/Menu.jsx'
+import Contact from './components/Contact.jsx'
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
  return (
 	<main>
-		<h1>Cocktail</h1>
+	 <Navbar />
+	 <Hero />
+	 <Cocktails />
+	 <About />
+	 <Art />
+	 <Menu />
+	 <Contact />
 	</main>
  )
 }
